8403036acf204963d86d0bdc454d8a32c37d356d
sakhnenkoff <39955656+sakhnenkoff@users.noreply.github.com>
2022-01-03 11:50:40 +0200

    Initial Commit

---
+++ Swifty Notes WatchKit Extension/Swifty_NotesApp.swift
@@ -0,0 +1,19 @@
+//
+//  Swifty_NotesApp.swift
+//  Swifty Notes WatchKit Extension
+//
+//  Created by Matthew Sakhnenko on 03.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct Swifty_NotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            NavigationView {
+                ContentView()
+            }
+        }
+    }
+}
